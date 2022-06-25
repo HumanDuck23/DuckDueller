6f5ddc9882b17be85529108c7ba9150e4fbeb1d1
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-06-25 12:32:09 +0200

    added option to get raw yaw and pitch difference, follow opponent but not perfectly

--- src/main/java/best/spaghetcodes/duckdueller/bot/player/Combat.java
+++ src/main/java/best/spaghetcodes/duckdueller/bot/player/Combat.java
@@ -6,6 +6,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.MathHelper;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class Combat {
     private static final Minecraft mc = Minecraft.getMinecraft();
@@ -13,9 +14,22 @@ public class Combat {
     private static boolean lookingLoop = false; // used to start/stop looking at the opponent
     private static int lookingInterval = 150;
 
-    public static void startLooking(int lookingInterval) {
+    // player tracking system: bot rotates with a fixed speed (will stutter a bit, not constant)
+    // every tick it will increase/decrease the yaw/pitch by lookSpeed depending on the two bools
+    private static float lookSpeedYaw = 0.1f;
+    private static float lookSpeedPitch = 0.1f;
+    private static float[] rotationsNeeded;
+
+    /**
+     * Start looking at the opponent
+     * @param lookingInterval Time between position updates
+     * @param lookSpeed How fast the bot will look around
+     */
+    public static void startLooking(int lookingInterval, float lookSpeed) {
         if (DuckDueller.INSTANCE.BOT.toggled) {
             Combat.lookingInterval = lookingInterval;
+            Combat.lookSpeedYaw = lookSpeed;
+            Combat.lookSpeedPitch = 0.1f;
             lookingLoop = true;
             Utils.runAfterTimeout(Combat::updateLooking, lookingInterval);
         }
@@ -24,9 +38,7 @@ public class Combat {
     private static void updateLooking() {
         if (DuckDueller.INSTANCE.BOT.opponent != null) {
             // TODO: make smooth transitions between positions
-            float[] rotations = getRotations(DuckDueller.INSTANCE.BOT.opponent);
-            mc.thePlayer.rotationYaw = rotations[0];
-            mc.thePlayer.rotationPitch = rotations[1];
+            rotationsNeeded = getRotations(DuckDueller.INSTANCE.BOT.opponent, true);
         } else {
             Utils.error("Error looking at opponent: NULL");
         }
@@ -40,9 +52,10 @@ public class Combat {
      * <br>
      * <i>Not originally my code, but I forgot where I found it.</i>
      * @param q entity
+     * @param raw If true, only returns difference in yaw and pitch instead of values needed
      * @return float[] - {yaw, pitch}
      */
-    public static float[] getRotations(Entity q) {
+    public static float[] getRotations(Entity q, boolean raw) {
         if (q == null) {
             return null;
         } else {
@@ -50,7 +63,7 @@ public class Combat {
             double diffY;
             if (q instanceof EntityLivingBase) {
                 EntityLivingBase en = (EntityLivingBase) q;
-                diffY = en.posY + (double) en.getEyeHeight() * 0.9D - (mc.thePlayer.posY + (double) mc.thePlayer.getEyeHeight());
+                diffY = en.posY + (double) en.getEyeHeight() - (mc.thePlayer.posY + (double) mc.thePlayer.getEyeHeight());
             } else {
                 diffY = (q.getEntityBoundingBox().minY + q.getEntityBoundingBox().maxY) / 2.0D - (mc.thePlayer.posY + (double) mc.thePlayer.getEyeHeight());
             }
@@ -59,7 +72,41 @@ public class Combat {
             double dist = MathHelper.sqrt_double(diffX * diffX + diffZ * diffZ);
             float yaw = (float) (Math.atan2(diffZ, diffX) * 180.0D / 3.141592653589793D) - 90.0F;
             float pitch = (float) (-(Math.atan2(diffY, dist) * 180.0D / 3.141592653589793D));
+            if (raw) {
+                return new float[]{MathHelper.wrapAngleTo180_float(yaw - mc.thePlayer.rotationYaw), MathHelper.wrapAngleTo180_float(pitch - mc.thePlayer.rotationPitch)};
+            }
             return new float[]{mc.thePlayer.rotationYaw + MathHelper.wrapAngleTo180_float(yaw - mc.thePlayer.rotationYaw), mc.thePlayer.rotationPitch + MathHelper.wrapAngleTo180_float(pitch - mc.thePlayer.rotationPitch)};
+        }
+    }
+
+    public static void onClientTick(TickEvent.ClientTickEvent ev) {
+        if (mc.thePlayer != null && DuckDueller.INSTANCE.BOT.toggled && lookingLoop && rotationsNeeded != null && rotationsNeeded.length == 2) {
+            if (Math.abs(rotationsNeeded[0]) > 40) {
+                if (rotationsNeeded[0] > 0) {
+                    mc.thePlayer.rotationYaw += lookSpeedYaw;
+                } else {
+                    mc.thePlayer.rotationYaw -= lookSpeedYaw;
+                }
+            } else if (Math.abs(rotationsNeeded[0]) > 20) {
+                if (rotationsNeeded[0] > 0) {
+                    mc.thePlayer.rotationYaw += 2.5;
+                } else {
+                    mc.thePlayer.rotationYaw -= 2.5;
+                }
+            } else if (Math.abs(rotationsNeeded[0]) > 5) {
+                if (rotationsNeeded[0] > 0) {
+                    mc.thePlayer.rotationYaw += 1.75;
+                } else {
+                    mc.thePlayer.rotationYaw -= 1.75;
+                }
+            }
+
+
+            if (rotationsNeeded[1] > 0) {
+                mc.thePlayer.rotationPitch += lookSpeedPitch;
+            } else {
+                mc.thePlayer.rotationPitch -= lookSpeedPitch;
+            }
         }
     }
 }
