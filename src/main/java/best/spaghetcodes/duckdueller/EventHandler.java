cad140f41811ce31bdd309bc5e77ee20a5d6a872
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-06-26 12:08:41 +0200

    call onHit() and onDamageTaken() methods

--- src/main/java/best/spaghetcodes/duckdueller/EventHandler.java
+++ src/main/java/best/spaghetcodes/duckdueller/EventHandler.java
@@ -1,20 +1,35 @@
 package best.spaghetcodes.duckdueller;
 
 import best.spaghetcodes.duckdueller.bot.player.Combat;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.lwjgl.input.Keyboard;
 
 public class EventHandler {
     private KeyBinding toggleBot = new KeyBinding("Toggle Bot", Keyboard.KEY_N, "key.categories.misc");
+    private final Minecraft mc = Minecraft.getMinecraft();
 
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent ev) {
         Combat.onClientTick(ev);
         if (toggleBot.isPressed()) {
             DuckDueller.INSTANCE.BOT.toggle();
+        }
+
+        if (mc.thePlayer != null && mc.thePlayer.maxHurtTime > 0 && mc.thePlayer.hurtTime == mc.thePlayer.maxHurtTime) {
+            DuckDueller.INSTANCE.BOT.onDamageTaken();
+        }
+    }
+
+    @SubscribeEvent
+    public void onAttackEntityEvent(AttackEntityEvent ev) {
+        if (ev.entity == mc.thePlayer) {
+            DuckDueller.INSTANCE.BOT.onHit();
         }
     }
 
