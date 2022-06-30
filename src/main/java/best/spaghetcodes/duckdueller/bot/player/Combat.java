2d096b9a7198674823ac5f916f738b4ba41d89bc
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2022-06-30 19:23:57 +0200

    Combat.getRotations now takes player and target as args

--- src/main/java/best/spaghetcodes/duckdueller/bot/player/Combat.java
+++ src/main/java/best/spaghetcodes/duckdueller/bot/player/Combat.java
@@ -5,6 +5,7 @@ import best.spaghetcodes.duckdueller.Utils;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.MathHelper;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
@@ -46,37 +47,37 @@ public class Combat {
      * <br>
      * <i>Not originally my code, but I forgot where I found it.</i>
      *
-     * @param q   entity
+     * @param target target entity
      * @param raw If true, only returns difference in yaw and pitch instead of values needed
      * @return float[] - {yaw, pitch}
      */
-    public static float[] getRotations(final Entity q, final boolean raw) {
-        if (q == null) {
+    public static float[] getRotations(final EntityPlayer player, final Entity target, final boolean raw) {
+        if (target == null || player == null) {
             return null;
         } else {
-            final double diffX = q.posX - mc.thePlayer.posX;
+            final double diffX = target.posX - player.posX;
             final double diffY;
-            if (q instanceof EntityLivingBase) {
-                final EntityLivingBase en = (EntityLivingBase) q;
-                diffY = en.posY + (double) en.getEyeHeight() - (mc.thePlayer.posY + (double) mc.thePlayer.getEyeHeight());
+            if (target instanceof EntityLivingBase) {
+                final EntityLivingBase en = (EntityLivingBase) target;
+                diffY = en.posY + (double) en.getEyeHeight() - (player.posY + (double) player.getEyeHeight());
             } else {
-                diffY = (q.getEntityBoundingBox().minY + q.getEntityBoundingBox().maxY) / 2.0D - (mc.thePlayer.posY + (double) mc.thePlayer.getEyeHeight());
+                diffY = (target.getEntityBoundingBox().minY + target.getEntityBoundingBox().maxY) / 2.0D - (player.posY + (double) player.getEyeHeight());
             }
 
-            final double diffZ = q.posZ - mc.thePlayer.posZ;
+            final double diffZ = target.posZ - player.posZ;
             final double dist = MathHelper.sqrt_double(diffX * diffX + diffZ * diffZ);
             final float yaw = (float) (Math.atan2(diffZ, diffX) * 180.0D / 3.141592653589793D) - 90.0F;
             final float pitch = (float) (-(Math.atan2(diffY, dist) * 180.0D / 3.141592653589793D));
             if (raw) {
-                return new float[]{MathHelper.wrapAngleTo180_float(yaw - mc.thePlayer.rotationYaw), MathHelper.wrapAngleTo180_float(pitch - mc.thePlayer.rotationPitch)};
+                return new float[]{MathHelper.wrapAngleTo180_float(yaw - player.rotationYaw), MathHelper.wrapAngleTo180_float(pitch - player.rotationPitch)};
             }
-            return new float[]{mc.thePlayer.rotationYaw + MathHelper.wrapAngleTo180_float(yaw - mc.thePlayer.rotationYaw), mc.thePlayer.rotationPitch + MathHelper.wrapAngleTo180_float(pitch - mc.thePlayer.rotationPitch)};
+            return new float[]{player.rotationYaw + MathHelper.wrapAngleTo180_float(yaw - player.rotationYaw), player.rotationPitch + MathHelper.wrapAngleTo180_float(pitch - player.rotationPitch)};
         }
     }
 
     public static void onClientTick(final TickEvent.ClientTickEvent ev) {
         if (mc.thePlayer != null && DuckDueller.INSTANCE.BOT.toggled && looking && DuckDueller.INSTANCE.BOT.opponent != null) {
-            final float[] rotations = getRotations(DuckDueller.INSTANCE.BOT.opponent, false);
+            final float[] rotations = getRotations(mc.thePlayer, DuckDueller.INSTANCE.BOT.opponent, false);
 
             if (changedYaw == -1 && !changingYawPositive) {
                 changedYawMax = Utils.randomIntInRange(-5, 5);
